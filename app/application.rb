72521dabc11fc5f69e07b2060440d894e31f5be5
ryancdavey1 <ryancdavey1@gmail.com>
2020-07-29 21:55:50 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -20,12 +20,13 @@ class Application
         resp.write "Your cart is empty"
       end
     elsif req.path.match(/add/)
-      add_term = req.params["q"]
-      if @@items.include?(add_term)
-        resp.write handle_search(add_term)
+      search_term = req.params["item"]
+      if @@items.include?(search_term)
+        resp.write "added #{search_term}"
       else
         resp.write "We don't have that item"
       end
+ 
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
